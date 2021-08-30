e1609f9e007e54398119fb82cbf7608fd34d6074
mantuonweb <mantuonweb@hotmail.com>
2021-08-30 14:29:35 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import HomeComppnent from './features/home/home.component';
 
 function App() {
   return (
-    <div className="App">
-      <HomeComppnent></HomeComppnent>
-    </div>
+    <>
+    <div className="main-container">
+        <HomeComppnent></HomeComppnent>
+      </div>
+
+      <div className="dev-sign text-center">
+        <b>Developed By :</b>
+        <br className=".d-none .d-xl-block line-break" />
+        <span>Mantu Nigam</span>
+      </div>
+    </>
   );
 }
 
